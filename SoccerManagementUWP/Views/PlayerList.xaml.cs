81ffef80e96db36e9e12b3679f863d702da1731f
Lloyd Niebel <Kulu-M@users.noreply.github.com>
2018-01-31 19:29:20 +0100

    First Version of PlayerList and PlayerProfile

---
+++ SoccerManagementUWP/Views/PlayerList.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SoccerManagementUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PlayerList : Page
+    {
+        public PlayerList()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var collection = App.IMongoDB.GetCollection<BsonDocument>("people");
+
+            var findFluent = collection.Find(_ => true).ToList();
+            var x = findFluent;
+
+            for (int i = 0; i < 100; i++)
+            {
+                lb_playerListBox.Items.Add((findFluent[i].GetValue("firstName").ToString()));
+            }
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
--- SoccerManagementUWP/Views/PlayerProfile.xaml.cs
+++ SoccerManagementUWP/Views/PlayerProfile.xaml.cs
@@ -12,33 +12,24 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Newtonsoft;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using SoccerManagementUWP.Views;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace SoccerManagementUWP
+namespace SoccerManagementUWP.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class PlayerProfile : Page
     {
-        public MainPage()
+        public PlayerProfile()
         {
             this.InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void b_showPlayerList_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(PlayerList));
+            Frame.GoBack();
         }
     }
 }
