c76d57b1d637296c1cd105770551277ea9ac2548
MartinVogelSRH <martin.vogel@stud.hochschule-heidelberg.de>
2018-02-02 03:23:49 +0100

    Make run command pretty again. Code is ugly, but it works

--- SoccerManagementUWP/Views/RunCommandTesting.xaml.cs
+++ SoccerManagementUWP/Views/RunCommandTesting.xaml.cs
@@ -14,6 +14,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using MongoDB.Bson.IO;
+using System.Data;
+using System.Text;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -31,11 +35,28 @@ namespace SoccerManagementUWP.Views
 
         private void btn_Execute_Click(object sender, RoutedEventArgs e)
         {
-            var command = new JsonCommand<BsonDocument>(tbx_command.Text);
+            var initialCommand = new JsonCommand<BsonDocument>(tbx_command.Text);
             try
             {
-                var result = App._IMongoDB.RunCommand(command);
-                tbx_Results.Text =  result.ToString();
+                StringBuilder resultText = new StringBuilder();
+                var result = App._IMongoDB.RunCommand(initialCommand).ToJson(new JsonWriterSettings {OutputMode = JsonOutputMode.Strict});
+
+                var firstResultStructure = new { cursor = new { firstBatch = "", id = "", ns = "" }, ok = "" };
+                //var firstResObject = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(result.ToString(),firstResultStructure);
+                var resultObject = Newtonsoft.Json.Linq.JObject.Parse(result.ToString());
+                resultText.Append(resultObject["cursor"]["firstBatch"]);
+
+                while (cbx_getAll.IsChecked == true && (resultObject["cursor"]["id"].ToString() != "0"))
+                {
+                    resultText.Remove(resultText.Length - 1, 1);
+                    resultText.Append(",");
+                    var getMoreCommand = new JsonCommand<BsonDocument>("{getMore : NumberLong(\"" + resultObject["cursor"]["id"].ToString() + "\"), collection : \"" + resultObject["cursor"]["ns"].ToString().Replace(Config.Configuration.mongoDatabaseName + ".","") + "\" }");
+                    var nextResult = App._IMongoDB.RunCommand(getMoreCommand).ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict });
+                    resultObject = Newtonsoft.Json.Linq.JObject.Parse(nextResult.ToString());
+                    resultText.Append(resultObject["cursor"]["nextBatch"].ToString().Remove(0,3));
+                }
+                    var prettyResult = Newtonsoft.Json.Linq.JToken.Parse(result.ToString()).ToString(Formatting.Indented);
+                tbx_Results.Text = resultText.ToString();
             }
             catch (Exception ex)
             {
@@ -47,7 +68,7 @@ namespace SoccerManagementUWP.Views
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            tbx_command.Text = "{ aggregate:'teams', pipeline: [ {$match: {city:'London'}} ] ,cursor:{} } ";
+            tbx_command.Text = "{ aggregate:'teams', pipeline: [ {$match: {city:'London'}} ] ,cursor:{batchSize:773} } ";
         }
     }
 }
