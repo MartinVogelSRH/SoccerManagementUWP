d2fc4ddb41f5471046b0dcef412307ee2b1b17db
Lloyd Niebel <Kulu-M@users.noreply.github.com>
2018-02-03 20:49:15 +0100

    Removed DB check

--- SoccerManagementUWP/App.xaml.cs
+++ SoccerManagementUWP/App.xaml.cs
@@ -45,11 +45,11 @@ namespace SoccerManagementUWP
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             _IMongoDB = MongoConnection.mongoConnect();
-            if (_IMongoDB == null)
-            {
-                Debug.WriteLine(Environment.NewLine + "No database donnection, shutting down." + Environment.NewLine);
-                Application.Current.Exit();
-            }
+            //if (_IMongoDB == null)
+            //{
+            //    Debug.WriteLine(Environment.NewLine + "No database donnection, shutting down." + Environment.NewLine);
+            //    Application.Current.Exit();
+            //}
             //usage:
             //var collection = App.IMongoDB.GetCollection<BsonDocument>("balls");
             //then use collection to query or insert
