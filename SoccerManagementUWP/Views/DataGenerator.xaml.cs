8f65bd9bd553134136cc441f2bec5cdab35b7fd7
MartinVogelSRH <martin.vogel@stud.hochschule-heidelberg.de>
2018-02-03 14:42:51 +0100

    Added trainers, agents, manager, competitions, games, awards to data generator

--- SoccerManagementUWP/Views/DataGenerator.xaml.cs
+++ SoccerManagementUWP/Views/DataGenerator.xaml.cs
@@ -75,10 +75,94 @@ namespace SoccerManagementUWP.Views
                 {"nationality","Germany" },
                 {"dateofbirth", new DateTime(1998,08,01,00,00,00) }
             };
-            collPeople.InsertOne(player1);
-            collPeople.InsertOne(player2);
-            collPeople.InsertOne(player3);
-            collPeople.InsertOne(player4);
+            var agent1 = new BsonDocument
+            {
+                {"type","agent" },
+                {"firstName", "Marcel" },
+                {"lastName", "Rosenbaum" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1970,03,18,00,00,00) }
+            };
+            var agent2 = new BsonDocument
+            {
+                {"type","agent" },
+                {"firstName", "Severin" },
+                {"lastName", "Steinitz" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1981,09,05,00,00,00) }
+            };
+            var agent3 = new BsonDocument
+            {
+                {"type","agent" },
+                {"firstName", "Julian" },
+                {"lastName", "Brockhaus" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1964,11,02,00,00,00) }
+            };
+            var trainer1 = new BsonDocument
+            {
+                {"type","trainer" },
+                {"firstName", "Eggert" },
+                {"lastName", "Oldenberg" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1964,10,19,00,00,00) }
+            };
+            var trainer2 = new BsonDocument
+            {
+                {"type","trainer" },
+                {"firstName", "Per" },
+                {"lastName", "Keller" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1970,12,13,00,00,00) }
+            };
+            var trainer3 = new BsonDocument
+            {
+                {"type","trainer" },
+                {"firstName", "Helge" },
+                {"lastName", "Kleinmann" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1975,11,27,00,00,00) }
+            };
+            var manager1 = new BsonDocument
+            {
+                {"type","manager" },
+                {"firstName", "Florian" },
+                {"lastName", "Eschenbach" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1960,01,28,00,00,00) }
+            };
+            var manager2 = new BsonDocument
+            {
+                {"type","manager" },
+                {"firstName", "Peter" },
+                {"lastName", "Gerson" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1964,07,12,00,00,00) }
+            };
+            var manager3 = new BsonDocument
+            {
+                {"type","manager" },
+                {"firstName", "Albin" },
+                {"lastName", "Neubauer" },
+                {"nationality","Germany" },
+                {"dateofbirth", new DateTime(1955,11,02,00,00,00) }
+            };
+            collPeople.InsertMany(new List<BsonDocument> {
+                    player1,
+                    player2,
+                    player3,
+                    player4,
+                    agent1,
+                    agent2,
+                    agent3,
+                    trainer1,
+                    trainer2,
+                    trainer3,
+                    manager1,
+                    manager2,
+                    manager3
+            });
+
             var mvupdatepl1 = new BsonDocument
             {
                 {"timestamp", new DateTime(2017,01,01,00,00,00) },
@@ -103,10 +187,13 @@ namespace SoccerManagementUWP.Views
                 {"value",2000000 },
                 {"playerId", player1["_id"]}
             };
-            collMvUpdate.InsertOne(mvupdatepl1);
-            collMvUpdate.InsertOne(mvupdatepl2);
-            collMvUpdate.InsertOne(mvupdatepl3);
-            collMvUpdate.InsertOne(mvupdatepl4);
+            collMvUpdate.InsertMany(new List<BsonDocument> {
+                mvupdatepl1,
+                mvupdatepl2,
+                mvupdatepl3,
+                mvupdatepl4
+            });
+
             var team1 = new BsonDocument
             {
                 {"name","AFC Bournemouth" },
@@ -121,9 +208,13 @@ namespace SoccerManagementUWP.Views
                 {"country","England" },
                 {"city","London" }
             };
-            collTeams.InsertOne(team1);
-            collTeams.InsertOne(team2);
-            var contract1 = new BsonDocument
+            collTeams.InsertMany(new List<BsonDocument>
+            {
+                team1,
+                team2
+            });
+
+            var contractplayer1 = new BsonDocument
             {
                 {"teamId",team1["_id"] },
                 {"playerId",player1["_id"] },
@@ -133,7 +224,7 @@ namespace SoccerManagementUWP.Views
                 {"currency","EUR" },
                 {"type","playerContract" }
             };
-            var contract2 = new BsonDocument
+            var contractplayer2 = new BsonDocument
             {
                 {"teamId",team2["_id"] },
                 {"playerId",player1["_id"] },
@@ -143,7 +234,7 @@ namespace SoccerManagementUWP.Views
                 {"currency","EUR" },
                 {"type","playerContract" }
             };
-            var contract3 = new BsonDocument
+            var contractplayer3 = new BsonDocument
             {
                 {"teamId",team2["_id"] },
                 {"playerId",player2["_id"] },
@@ -153,7 +244,7 @@ namespace SoccerManagementUWP.Views
                 {"currency","EUR" },
                 {"type","playerContract" }
             };
-            var contract4 = new BsonDocument
+            var contractplayer4 = new BsonDocument
             {
                 {"teamId",team1["_id"] },
                 {"playerId",player3["_id"] },
@@ -163,7 +254,7 @@ namespace SoccerManagementUWP.Views
                 {"currency","EUR" },
                 {"type","playerContract" }
             };
-            var contract5 = new BsonDocument
+            var contractplayer5 = new BsonDocument
             {
                 {"teamId",team1["_id"] },
                 {"playerId",player4["_id"] },
@@ -173,13 +264,182 @@ namespace SoccerManagementUWP.Views
                 {"currency","EUR" },
                 {"type","playerContract" }
             };
+            var contractAgent1 = new BsonDocument
+            {
+                {"agentId",agent1["_id"] },
+                {"playerId",player4["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractAgent2 = new BsonDocument
+            {
+                {"agentId",agent2["_id"] },
+                {"playerId",player3["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractAgent3 = new BsonDocument
+            {
+                {"agentId",agent3["_id"] },
+                {"playerId",player2["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractAgent4 = new BsonDocument
+            {
+                {"agentId",agent1["_id"] },
+                {"playerId",player1["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractTrainer1 = new BsonDocument
+            {
+                {"trainerId",trainer1["_id"] },
+                {"clubId",team1["_id"] },
+                {"startDate",new DateTime(2015,01,01,00,00,00) },
+                {"endDate",new DateTime(2016,12,31,00,00,00) },
+                {"type","trainerContract" }
+            };
+            var contractTrainer2 = new BsonDocument
+            {
+                {"trainerId",trainer2["_id"] },
+                {"clubId",team1["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","trainerContract" }
+            };
+            var contractTrainer3 = new BsonDocument
+            {
+                {"trainerId",trainer2["_id"] },
+                {"clubId",team2["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","trainerContract" }
+            };
+            var contractManager1 = new BsonDocument
+            {
+                {"managerId",manager1["_id"] },
+                {"clubId",team1["_id"] },
+                {"startDate",new DateTime(2015,01,01,00,00,00) },
+                {"endDate",new DateTime(2016,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractManager2 = new BsonDocument
+            {
+                {"managerId",manager2["_id"] },
+                {"clubId",team1["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            var contractManager3 = new BsonDocument
+            {
+                {"managerId",manager3["_id"] },
+                {"clubId",team2["_id"] },
+                {"startDate",new DateTime(2017,01,01,00,00,00) },
+                {"endDate",new DateTime(2019,12,31,00,00,00) },
+                {"type","agentContract" }
+            };
+            collContracts.InsertMany(new List<BsonDocument> {
+                contractplayer1,
+                contractplayer2,
+                contractplayer3,
+                contractplayer4,
+                contractplayer5,
+                contractAgent1,
+                contractAgent2,
+                contractAgent3,
+                contractAgent4,
+                contractTrainer1,
+                contractTrainer2,
+                contractTrainer3,
+                contractManager1,
+                contractManager2,
+                contractManager3
+            });
 
-            collContracts.InsertOne(contract1);
-            collContracts.InsertOne(contract2);
-            collContracts.InsertOne(contract3);
-            collContracts.InsertOne(contract4);
-            collContracts.InsertOne(contract5);
+            var awardManagerMonth1 = new BsonDocument {
+                {"managerId", manager1["_id"] },
+                {"awardType","managerOfTheMonthAward" },
+                {"timestamp", new DateTime(2017,10,01)}
+            };
+            var awardManagerMonth2 = new BsonDocument {
+                {"managerId", manager2["_id"] },
+                {"awardType","managerOfTheMonthAward" },
+                {"timestamp", new DateTime(2017,11,01)}
+            };
+            var awardManagerMonth3 = new BsonDocument {
+                {"managerId", manager3["_id"] },
+                {"awardType","managerOfTheMonthAward" },
+                {"timestamp", new DateTime(2017,12,01)}
+            };
+            var awardManagerMonth4 = new BsonDocument {
+                {"managerId", manager1["_id"] },
+                {"awardType","managerOfTheMonthAward" },
+                {"timestamp", new DateTime(2018,01,01)}
+            };
+            collPeople.InsertMany(new List<BsonDocument>
+            {
+                awardManagerMonth1,
+                awardManagerMonth2,
+                awardManagerMonth3,
+                awardManagerMonth4
+            });
+
+            var competition1 = new BsonDocument
+            {
+                {"type","club" },
+                {"format","Domestic League" },
+                {"name", "Dummy League" },
+                {"season","2017" },
+                {"country","germany" },
+                {"teams", new BsonArray{team1["_id"],team2["_id"] } }
+            };
+            collCompetitions.InsertMany(new List<BsonDocument>
+            {
+                competition1
+            });
+
+            var game1 = new BsonDocument {
+                {"competitionId",competition1["_id"] },
+                {"awayTeamId",team1["_id"] },
+                {"homeTeamId",team2["_id"] },
+                {"startTime", new DateTime(2017,03,26,15,30,00) }
+            };
+            var game2 = new BsonDocument {
+                {"competitionId",competition1["_id"] },
+                {"awayTeamId",team2["_id"] },
+                {"homeTeamId",team1["_id"] },
+                {"startTime", new DateTime(2017,09,03,20,45,00) }
+            };
+            collGames.InsertMany(new List<BsonDocument>
+            {
+                game1,
+                game2
+            });
+
+            var awardManOfTheMatch1 = new BsonDocument {
+                {"playerId", player1["_id"] },
+                {"awardType","manOfTheMatchAward" },
+                {"game", game1["_id"]}
+            };
+            var awardManOfTheMatch2 = new BsonDocument {
+                {"playerId", player2["_id"] },
+                {"awardType","manOfTheMatchAward" },
+                {"game", game2["_id"]}
+            };
+            collPeople.InsertMany(new List<BsonDocument>
+            {
+                awardManOfTheMatch1,
+                awardManOfTheMatch2,
+            });
+
         }
+
 
         private void btn_back_Click(object sender, RoutedEventArgs e)
         {
